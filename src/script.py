0fd849e5d78b2b5c1184691419815dc3573dedf2
hernanjkd <hernanjkd@gmail.com>
2019-12-28 17:09:48 -0500

    zip codes in json file

--- src/script.py
+++ src/script.py
@@ -3,14 +3,13 @@ import json
 
 data = requests.get('https://assets.breatheco.de/apis/fake/zips.php').json()
     
-new = []
+new = {}
 
 for zip in data:
-    new.append({
-        'zip_code': zip['_id'],
+    new[zip['_id']] = {
         'longitude': zip['loc'][0],
         'latitude': zip['loc'][1]
-    })
+    }
 
 
 with open('src/zip_codes.json', 'w') as f:
@@ -18,3 +17,6 @@ with open('src/zip_codes.json', 'w') as f:
     
 
 print('ok')
+
+# with open('src/zip_codes.json') as f:
+#     print(json.load(f)['33185'])
