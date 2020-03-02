f9036ae9eb8da80d18994f295c40f07ca9187094
hernanjkd <hernanjkd@gmail.com>
2020-03-01 19:17:42 -0500

    seeds updated

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import re
 import io
 import csv
 import utils
+import seeds
 import actions
 import requests
 import pandas as pd
@@ -59,6 +60,13 @@ def jwt():
 #         'sub': id,
 #         'role': role
 #     }
+
+
+
+@app.route('/reset_database')
+def reset_database():
+    seeds.run()
+    return 'seeds ran'
 
 
 
@@ -156,104 +164,30 @@ def file_upload():
     # POST
     if 'excel' not in request.files:
         raise APIException('"excel" property missing in the files array', 404)
-    # Flights.query.delete()
-    # Tournaments.query.delete()
-    # db.session.execute('ALTER SEQUENCE flights_id_seq RESTART')
-    # db.session.execute('ALTER SEQUENCE tournaments_id_seq RESTART')
-    # db.session.commit()
-    # return 'done'
+
     f = request.files['excel']
     df = pd.read_excel( f, keep_default_na=False )
-    print(list(df));return 'song'
-    # file_header: db_column. Used to loop and check properties quicker
-    trmnt_ref = {'Buy-in':'buy_in','Starting Stack':'starting_stack','Blinds':'blinds',
-        'H1':'h1','Structure Link':'structure_link',#'Casino ID':'casino_id', 
-        'Results Link':'results_link','Multi ID':'multiday_id'}
     
-    added_id_to_file = False
-    error_list = []
+    headers = list(df)
 
-    for index, r in df.iterrows():
-        
-        if r['Tournament'].strip() == '':
-            continue
+    if utils.are_headers_for('tournament', headers):
 
-        trmnt_name, flight_day = utils.resolve_name_day( r['Tournament'] )
-        start_at = datetime.combine( r['Date'].to_pydatetime(), r['Time'] )
+        updated_df, error_list = actions.process_tournament_excel( df )
 
-        # Used to loop and check properties quicker
-        flight_ref = { 'start_at': start_at, 'day': flight_day,
-            'notes': r['NOTES - LOU'].strip() }
+        # Update Swap Profit
+        swapprofit = Subscribers.query.filter_by(company_name='Swap Profit').first()
+        if swapprofit is not None:
+            requests.post(
+                swapprofit.api_host +'/tournaments',
+                headers = {'Authorization': 'Bearer '+ swapprofit.api_token},
+                json = updated_df.to_json(orient='records')
+            )
+        else: error_list.append('Swap Profit not a subscriber')
 
+        if len(error_list) > 0:
+            return jsonify(error_list)
 
-        # If the tournament id hasn't been saved, it could be a new tournament
-        if str(r['Tournament ID']).strip() == '':
-
-            if flight_day is not None:              
-                # Check to see if trmnt has been saved already
-                trmnt = Tournaments.query.filter_by(
-                    multiday_id = r['Multi ID'].strip() ).first()
-            
-            if flight_day is None or trmnt is None:
-                trmnt = Tournaments(
-                    name = trmnt_name,
-                    start_at = start_at,
-                    **{ db_column: str(r[file_header]).strip()
-                        for file_header, db_column in trmnt_ref.items() }
-                )
-                db.session.add( trmnt )
-                db.session.commit()
-            
-            db.session.add( Flights(
-                tournament_id = trmnt.id,
-                **flight_ref
-            ))
-            db.session.commit()
-            
-            # save trmnt.id in the file
-            df.at[index,'Tournament ID'] = trmnt.id
-            added_id_to_file = True
-
-        
-        else:
-            trmnt = Tournaments.query.get( r['Tournament ID'] )
-            if trmnt is None:
-                error_list.append(f'Can\'t find Tournament id: "{r["Tournament ID"]}"')
-                continue
-            flight = Flights.query.filter_by( tournament_id=trmnt.id, day=flight_day ).first()
-            if flight is None:
-                error_list.append(
-                    f'Can\'t find Flight tournament_id: {trmnt.id}, day: {flight_day}' )
-                continue
-
-            for db_column, value in flight_ref.items():
-                if getattr(flight, db_column) != value:
-                    setattr( flight, db_column, value )
-
-            first_day = ['1', '1A']
-            if flight_day in first_day:
-                for file_header, db_column in trmnt_ref.items():
-                    entry = str(r[file_header]).strip()
-                    if getattr(trmnt, db_column) != entry:
-                        setattr( trmnt, db_column, entry )            
-                if trmnt.start_at != start_at:
-                    trmnt.start_at = start_at
-                if trmnt.name != trmnt_name:
-                    trmnt.name = trmnt_name
-
-            db.session.commit()
-
-
-    if added_id_to_file:
-        writer = pd.ExcelWriter(
-            os.path.join('/Users/Francine/Desktop/csv/processed csv/', f.filename) )
-        df.to_excel(writer, index=False)
-        writer.save()
-
-    if len(error_list) > 0:
-        return jsonify(error_list)
-
-    return 'Done'
+        return 'Done'
 
     
     # Tournaments
@@ -397,7 +331,6 @@ def get_roi_data(email):
                     .filter( Results.earnings != None )
     
     return jsonify(won_trmnts.count()), 200
-
 
 
 
--- src/seeds.py
+++ src/seeds.py
@@ -18,31 +18,6 @@ def run():
     db.session.execute("ALTER SEQUENCE results_id_seq RESTART")
     db.session.execute("ALTER SEQUENCE subscribers_id_seq RESTART")
 
-    
-    c1 = Casinos(
-        name = 'Seminole Hard Rock Hollywood',
-        address = '1234 SW 57 Ave',
-        city = 'Hollywood',
-        state = 'FL',
-        zip_code = '',
-        longitude = 0,
-        latitude = 0,
-        website = '',
-        h1 = ''
-    )
-
-    t1 = Tournaments(
-        casino = c1,
-        name = 'Hard Rock Night $400 Guaranteed',
-        buy_in = '',
-        blinds = 0,
-        starting_stack = 0,
-        results_link = '',
-        structure_link = '',
-        start_at = datetime.strptime('1-Dec-19 9:00 AM', '%d-%b-%y %I:%M %p'),
-        notes = ''
-    )
-    db.session.add_all([c1, t1])
 
     db.session.add( Subscribers(
         company_name = 'Swap Profit',
