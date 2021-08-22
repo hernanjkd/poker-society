9e70634aee158e8cd24a0edc740c07d5fc1b0c1b
GGabe <techpriest.gabriel@gmail.com>
2021-08-22 13:52:53 -0400

    dcd

--- src/actions.py
+++ src/actions.py
@@ -248,11 +248,10 @@ def process_results_excel(df):
     db.session.commit()
 
     theSubscriber = Subscribers.query.filter_by(company_name=str(r['Subscriber'])).first()
+    print("HEKKIIII")
     if theSubscriber is None:
         return 'No one is a subscriber'
-    print(theSubscriber, trmnt_data,{
-        'message': 'Results excel processed successfully'
-    })
-    return theSubscriber, trmnt_data, {
-        'message': 'Results excel processed successfully'
-    }
+    x = {'message': 'Results excel processed successfully'}
+
+    print(theSubscriber, trmnt_data, x)
+    return theSubscriber, trmnt_data, x
