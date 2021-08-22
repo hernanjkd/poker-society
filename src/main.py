2a53994d571e23f6d063cc535b1960c064f93824
GGabe <techpriest.gabriel@gmail.com>
2021-08-22 14:07:03 -0400

    vvv

--- src/main.py
+++ src/main.py
@@ -195,7 +195,7 @@ def file_upload():
             # SWAPPROFUT ENDS HERE  swapprofit.api_host
             resp = requests.post( subscriber.api_host + '/results/update',
                 json={
-                    'api_token': os.environ[api_token] ,
+                    'api_token': os.environ.get[api_token] ,
                     **subscriber_json
                 })
 
